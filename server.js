733d83fb24f4b760cc7986e01631d98934b707b1
president <abanda435@gmail.com>
2024-01-13 17:20:30 +0200

    added search from jason placeholder

--- server.js
+++ server.js
@@ -96,8 +96,42 @@ app.post("/search", (req, res)=>{
         const searchResultHtml = searchResults.map( contact => `
             <tr>
                 <td>
-                    <div className="my-4 p-2">${contact.name}</div>
-                    <div className="my-4 p-2">${contact.email}</div>
+                    <div class="my-4 p-2">${contact.name}</div>
+                    <div class="my-4 p-2">${contact.email}</div>
+                </td>
+            </tr>
+        `).join('')
+
+        res.send(searchResultHtml);
+    }, 1000);
+});
+
+// Handle POST request for json placeholder
+app.post("/search/api", async (req, res)=>{
+    const searchTerm = req.body.search.toLowerCase();
+
+    if(!searchTerm){
+        return res.send('<tr></tr>');
+    }
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+
+    const contacts = await response.json();
+
+    const searchResults = contacts.filter(contact => {
+        const name = contact.name.toLowerCase();
+        const email = contact.email.toLowerCase();
+
+        return name.includes(searchTerm) || email.includes(searchTerm);
+    
+    })
+
+    setTimeout(() => {
+        const searchResultHtml = searchResults.map( contact => `
+            <tr>
+                <td>
+                    <div class="my-4 p-2">${contact.name}</div>
+                    <div class="my-4 p-2">${contact.email}</div>
                 </td>
             </tr>
         `).join('')
